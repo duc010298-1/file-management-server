87eefe57a1ed4c9a3da35f3e1a98d9dabd8a3b57
Do Trung Duc <duc010298@gmail.com>
2022-12-26 16:32:55 +0700

    update source code

--- transfer_file_app/models.py
+++ transfer_file_app/models.py
@@ -1,13 +1,27 @@
 import os
+import uuid
 from django.db import models
 from django.dispatch import receiver
 
 
 class FileUpload(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+    file_name = models.TextField(blank=True, null=True)
+    file_size = models.PositiveIntegerField(blank=True, null=True)
     file = models.FileField()
-    file_size = models.PositiveIntegerField()
-    uploaded_by = models.ForeignKey('auth.User', related_name='file_uploads', on_delete=models.PROTECT)
+    uploaded_by = models.ForeignKey(
+        'auth.User', related_name='file_uploads', on_delete=models.PROTECT
+    )
+
+    def save(self, *args, **kwargs):
+        if self.file:
+            self.file_name = self.file.name
+            self.file_size = self.file.size
+            file_name, file_extension = os.path.splitext(self.file_name)
+            self.file.name = '{}_{}{}'.format(
+                file_name, uuid.uuid4().hex, file_extension)
+
+        super(FileUpload, self).save(*args, **kwargs)
 
 
 @receiver(models.signals.post_delete, sender=FileUpload)
