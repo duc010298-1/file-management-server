0fcde238d970d3ff4cfe6efed424485020ba8f33
Do Trung Duc <duc010298@gmail.com>
2022-12-27 17:55:02 +0700

    update source code

--- file_management_app/module/file/get_list_file.py
+++ file_management_app/module/file/get_list_file.py
@@ -41,15 +41,19 @@ class GetListFileView(APIView, PageNumberPagination):
         PageNumberPagination.page_size = page_size_int
 
         files = File.objects.filter(owner=user)
-        results_files = self.paginate_queryset(
-            files, request, view=self)
+        try:
+            results_files = self.paginate_queryset(files, request, view=self)
+            count = self.page.paginator.count
+        except:
+            results_files = []
+            count = 0
 
         fields = ('id', 'file_name', 'file_size', 'created')
         serializer = FileSerializer(
             results_files, many=True, fields=fields
         )
         return Response({
-            'count': self.page.paginator.count,
+            'count': count,
             'page_size': page_size_int,
             'page_number': page_int,
             'results': serializer.data
