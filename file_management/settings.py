e6eb9c50f13c42ac50c5f976ff4a14a436a1f55a
DoTrungDuc <duc010298@gmail.com>
2023-01-23 17:33:17 +0700

    update source code

--- file_management/settings.py
+++ file_management/settings.py
@@ -94,10 +94,11 @@ WSGI_APPLICATION = 'file_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_FILE = os.environ.get('DATABASE_FILE', default=BASE_DIR / 'db.sqlite32')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DATABASE_FILE,
     }
 }
 
