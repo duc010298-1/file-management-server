fc17e4ffa51330a0fad2eba4104f8341b6ab60c1
DoTrungDuc <duc010298@gmail.com>
2023-01-24 09:28:44 +0700

    update

--- file_management/urls.py
+++ file_management/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
